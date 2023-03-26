42531eead6b9f3ad35805f297fd1bfdccaf29ffe
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 01:34:02 +0100

    Dev/Feat: Add two functions : - get all projects of already started sprints - get all projects of a user by firstname and lastname and if user's role is scrum master

--- src/main/java/com/esprit/exam/repositories/ProjectRepository.java
+++ src/main/java/com/esprit/exam/repositories/ProjectRepository.java
@@ -1,7 +1,13 @@
 package com.esprit.exam.repositories;
 
 import com.esprit.exam.entities.Project;
+import com.esprit.exam.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    List<Project> findAllBySprintsStartDateLessThan(Date date);
+    List<Project> findAllByUsersRoleAndUsersFnameAndUsersLname(Role role, String fname, String lname);
 }
