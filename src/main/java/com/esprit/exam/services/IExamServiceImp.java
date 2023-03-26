88ba5e628f8d8a8bd520841279ffc75a35632a99
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 01:31:01 +0100

    Dev/Feat: Merge Services into ExamService Add functions : assignProjectToUser, assignProjectToClient, getAllCurrentProject, getProjectsByScrumMaster

---
+++ src/main/java/com/esprit/exam/services/IExamServiceImp.java
@@ -0,0 +1,142 @@
+package com.esprit.exam.services;
+
+
+import com.esprit.exam.entities.Project;
+import com.esprit.exam.entities.Role;
+import com.esprit.exam.entities.Sprint;
+import com.esprit.exam.entities.User;
+import com.esprit.exam.repositories.ProjectRepository;
+import com.esprit.exam.repositories.SprintRepository;
+import com.esprit.exam.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class IExamServiceImp implements IExamServices{
+
+
+    private final UserRepository userRepository;
+    private final SprintRepository sprintRepository;
+    private final ProjectRepository projectRepository;
+    @Override
+    public List<Project> retrieveAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @Override
+    public Project updateProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    @Override
+    @Transactional
+    public Project addProject(Project project) throws Exception {
+        if(project.getSprints() == null || project.getSprints().size() == 0){
+            throw new Exception("You must provide at least provide one sprint");
+        }
+        projectRepository.saveAndFlush(project);
+        project.getSprints().forEach((s) -> {
+            s.setProject(project);
+        });
+        return project;
+    }
+
+    @Override
+    public Project retrieveProject(int id) {
+        return projectRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void removeProject(int id) {
+        projectRepository.deleteById(id);
+    }
+    @Override
+    public List<Sprint> retrieveAllSprints() {
+        return sprintRepository.findAll();
+    }
+
+    @Override
+    public Sprint updateSprint(Sprint sprint) {
+        return sprintRepository.save(sprint);
+    }
+
+    @Override
+    public Sprint addSprint(Sprint sprint) {
+        return sprintRepository.save(sprint);
+    }
+
+    @Override
+    public Sprint retrieveSprint(Integer id) {
+        return sprintRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void removeSprint(Integer id) {
+        sprintRepository.deleteById(id);
+    }
+    @Override
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User retrieveUser(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void removeUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void assignProjectToUser(int projectId, int userId) {
+        Project project = projectRepository.findById(projectId).orElse(null);
+        User user = userRepository.findById(userId).orElse(null);
+        Assert.notNull(user, "User must be not null");
+        Assert.notNull(project, "Project must be not null");
+        user.getProjects().add(project);
+        user.setProjects(user.getProjects());
+    }
+
+    @Override
+    @Transactional
+    public void assignProjectToClient(int projectId, String fname, String lname) {
+        Project project = projectRepository.findById(projectId).orElse(null);
+        User client = userRepository.findByFnameAndLname(fname, lname).orElse(null);
+        Assert.notNull(client, "User must be not null");
+        Assert.notNull(project, "Project must be not null");
+        client.getClient_projects().add(project);
+        client.setClient_projects(client.getClient_projects());
+    }
+
+    @Override
+    public List<Project> getAllCurrentProjects() {
+        Date date = new Date();
+        return projectRepository.findAllBySprintsStartDateLessThan(date);
+    }
+
+    @Override
+    public List<Project> getProjectsByScrumMaster(String fname, String lname) {
+        return projectRepository.findAllByUsersRoleAndUsersFnameAndUsersLname(Role.SCRUM_MASTER,fname, lname);
+    }
+
+
+}
---
+++ src/main/java/com/esprit/exam/services/IExamServices.java
@@ -0,0 +1,35 @@
+package com.esprit.exam.services;
+
+import com.esprit.exam.entities.Project;
+import com.esprit.exam.entities.Sprint;
+import com.esprit.exam.entities.User;
+
+import java.util.List;
+
+public interface IExamServices {
+    List<Project> retrieveAllProjects();
+    Project updateProject(Project project);
+    Project addProject(Project project) throws Exception;
+    Project retrieveProject(int id);
+    void removeProject(int id);
+
+    List<Sprint> retrieveAllSprints();
+    Sprint updateSprint(Sprint sprint);
+    Sprint addSprint(Sprint sprint);
+    Sprint retrieveSprint(Integer id);
+    void removeSprint(Integer id);
+
+    List<User> retrieveAllUsers();
+    User updateUser(User user);
+    User addUser(User user);
+    User retrieveUser(int id);
+    void removeUser(int id);
+
+    void assignProjectToUser(int projectId, int userId);
+
+    void assignProjectToClient(int projectId, String fname, String lname);
+
+    List<Project> getAllCurrentProjects();
+
+    List<Project> getProjectsByScrumMaster(String fname, String lname);
+}
