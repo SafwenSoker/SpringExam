845dcb58e5e59bac1e08b78fcf061c966569b688
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 01:35:51 +0100

    Dev/Feat: Add function to get user by firstname and lastname

--- src/main/java/com/esprit/exam/repositories/UserRepository.java
+++ src/main/java/com/esprit/exam/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.esprit.exam.repositories;
 import com.esprit.exam.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByFnameAndLname(String fname, String lname);
 }
